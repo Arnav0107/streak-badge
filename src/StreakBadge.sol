ac0de569480997d6dbd7147ebaeff5bf88019d7b
Arnav0107 <arnavbansal.edu@gmail.com>
2026-07-21 20:10:33 +0000

    Initial commit: StreakBadge - evolving ERC721 attendance badge

---
+++ src/StreakBadge.sol
@@ -0,0 +1,43 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import {ERC721} from "@openzeppelin/contracts/token/ERC721/ERC721.sol";
+
+
+contract StreakBadge is ERC721 {
+
+    address public organizer;
+    uint256 private nextTokenId;
+
+    mapping(address=> uint256) public tokenIdOf;
+
+    mapping(address => uint256) public attendanceCount;
+
+    mapping(address => mapping(uint256 => bool)) public hasAttendedEvent;
+
+    error NotOrganizer();
+    error AlreadyCheckedIn();
+
+    modifier onlyOrganizer() {
+        if (msg.sender != organizer) revert NotOrganizer();
+        _;
+    }
+
+
+    constructor() ERC721("Streak Badge", "STREAK") {
+
+    }
+    function checkIn(address attendee, uint256 eventId) external onlyOrganizer {
+        if (hasAttendedEvent[attendee][eventId]) revert AlreadyCheckedIn();
+
+        hasAttendedEvent[attendee][eventId] = true;
+        attendanceCount[attendee]++;
+
+        if (tokenIdOf[attendee] == 0) {
+            nextTokenId++;
+            tokenIdOf[attendee] = nextTokenId;
+            _safeMint(attendee, nextTokenId);
+    }
+}
+
+}
